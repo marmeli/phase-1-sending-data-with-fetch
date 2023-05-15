2ef43c84cda0efc37ccae1e5cfb452ffaa6377bb
melissamariano <melissams538@gmail.com>
2023-05-15 18:31:53 -0400

    sending data with fetch

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Add your code here
+function submitData(name, email){
+    const formData = {
+        name: name,
+        email: email,
+    };
+    const configurationObject = {
+    method: "POST",
+    headers: {
+      "content-type": "application/json",
+      "accept": "application/json",
+    },
+    body: JSON.stringify(formData),
+  };
+  
+  return fetch("http://localhost:3000/users", configurationObject)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (object) {
+    const newId = object.id;
+    document.body.innerHTML += newId;
+  })
+  .catch(function (error) {
+    document.body.innerHTML += error.message;
+  });
+}
